a5fe4e0aa5a44b07617b7904658a77f1f4e5fc52
Martin McLaren <61330791+LuckyMiner01@users.noreply.github.com>
2021-11-10 16:02:26 +0000

    Add files via upload

---
+++ Area of a rectangle WinForms with file output/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Area_of_a_rectangle_WinForms__2_
+{
+    public partial class Area_of_a_Rectangle_Calculator : Form
+    {
+        public Area_of_a_Rectangle_Calculator()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listbox_predefined_units.Enabled = false;
+            txtbox_custom_unit.Enabled = false;
+            //btn_calculate.Enabled = false;
+        }
+
+        private void btn_help_Click(object sender, EventArgs e)
+        {
+            //adds a label, telling the use how to use the app
+            Label lbl_help = new Label();
+            lbl_help.Text = "Input the width of the rectangle in the first box and the height of the rectangle in the second box. You can also define you own unit if you wish, but you can also chose from a premade one.";
+            lbl_help.Location = new Point(12, 408);
+            lbl_help.Size = new Size(259, 52);
+            lbl_help.TextAlign = ContentAlignment.TopCenter;
+            //lbl_help.Font = new Font("Microsoft Sans Serif", 8);
+            //lbl_help.Font = new System.Drawing.Font(lbl_help.Font.Name, ); //Changes the size of the font
+            this.Controls.Add(lbl_help);
+
+            //resizes the win form to fit hte help label
+            this.Size = new Size(545, 516);
+        }
+
+        private void btn_calculate_Click(object sender, EventArgs e)
+        {
+            //getting units
+            string units = "placeholder";
+            if (radbtn_custom_unit.Checked == true)
+            {
+                units = txtbox_custom_unit.Text;
+            }
+            else if (radbtn_predefined_unit.Checked == true)
+            {
+                units = Convert.ToString(listbox_predefined_units.SelectedItem);
+            }
+            else
+            {
+                lbl_error_output.Text = "Units not changed";
+            }
+
+            //calculating area
+            double area_calculated = double.Parse(txtbox_width.Text) * double.Parse(txtbox_height.Text);
+            string area_calculated_string = Convert.ToString(area_calculated);
+            
+            //concatenating units to area
+            string area_with_units = area_calculated_string + units + "²";
+
+            //
+            lbl_area_output.Text = Convert.ToString(area_with_units);
+
+            //Checking if the user wants to output to a file
+            if (chkbox_write_to_file.Checked == true)
+            {
+                string path = txtbox_path.Text;
+                string file = txtbox_file_name.Text;
+                using (StreamWriter outputFile = new StreamWriter(Path.Combine(path, file)))
+                {
+                    outputFile.WriteLineAsync(area_with_units);
+                }
+
+            }
+        }
+
+        private void radbtn_predefined_unit_CheckedChanged(object sender, EventArgs e)
+        {
+            //PREDEFINED UNITS RADBUTTON
+            listbox_predefined_units.Enabled = true;
+            txtbox_custom_unit.Enabled = false;
+
+        }
+
+        private void radbtn_custom_unit_CheckedChanged(object sender, EventArgs e)
+        {
+            //CUSTOM UNITS RADBUTTON
+            listbox_predefined_units.Enabled = false;
+            txtbox_custom_unit.Enabled = true;
+        }
+
+        private void chkbox_write_to_file_CheckedChanged(object sender, EventArgs e)
+        {
+            txtbox_path.Enabled = true;
+            txtbox_file_name.Enabled = true;
+        }
+
+        private void txtbox_width_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtbox_height_TextChanged(object sender, EventArgs e)
+        {
+            if (txtbox_width.Text != "")
+            {
+                listbox_predefined_units.Enabled = false;
+                txtbox_custom_unit.Enabled = false;
+            }
+        }
+    }
+}
